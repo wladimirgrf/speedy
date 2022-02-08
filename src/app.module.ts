fe62ca4339c19522174f60af1e428d3889accacf
wladimirgrf <wladimir.code@gmail.com>
2022-02-08 14:18:47 -0300

    feat: nest config module, env properties

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
