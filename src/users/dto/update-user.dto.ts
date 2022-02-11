1b51ffde2f0be9e30e369d66b72d50c857e9371e
wladimirgrf <wladimir.code@gmail.com>
2022-02-11 16:03:36 -0300

    fix: UpdateUserDTO, partial type

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,4 @@
-import { User } from '../entities/user.entity';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends User {
-  email: string;
-  name: string;
-  password: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
