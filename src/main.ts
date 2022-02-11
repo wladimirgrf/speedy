313ddcaa5ac19509099f4293243857eb9d89255a
wladimirgrf <wladimir.code@gmail.com>
2022-02-11 16:13:07 -0300

    feat: validation request, class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const validation = new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  });
+
+  app.useGlobalPipes(validation);
+
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,13 @@
+import { IsEmail, IsString } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends User {
+  @IsEmail()
   email: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
   password: string;
 }
