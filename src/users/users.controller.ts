f76b42e7d79412c840304efa87c08b627bd5bd9d
wladimirgrf <wladimir.code@gmail.com>
2022-02-14 18:49:28 -0300

    fix: users controller, findOne to findById

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,8 +26,8 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  findById(@Param('id') id: string) {
+    return this.usersService.findById(id);
   }
 
   @Patch(':id')
